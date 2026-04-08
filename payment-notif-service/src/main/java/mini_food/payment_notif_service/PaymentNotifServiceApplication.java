d04d8b0963d9b9438db1b489d2647dad74bf9c7a
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2026-04-08 18:28:14 +0700

    first commmit

---
+++ payment-notif-service/src/main/java/mini_food/payment_notif_service/PaymentNotifServiceApplication.java
@@ -0,0 +1,13 @@
+package mini_food.payment_notif_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentNotifServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentNotifServiceApplication.class, args);
+	}
+
+}
